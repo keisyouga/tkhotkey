4070ea10d8587bddab7c726d239174d965088089
keisyouga <keisyouga@users.noreply.github.com>
2022-05-30 21:38:25 +0900

    check if keycode and modifiers are in the range.

--- hotkey.c
+++ hotkey.c
@@ -9,7 +9,15 @@
 /* dict of hotkey/script pairs */
 static Tcl_Obj *hotkeyInfo;
 
-/* eval script if hotkey, otherwise through the event */
+/* pass this to XGrabKey(), XUngrabKey() */
+struct XGrabkeyInfo {
+	Display *display;
+	int keycode;
+	unsigned int modifiers;
+	Window grab_window;
+};
+
+/* eval script if pressed key is hotkey, otherwise through the event */
 static int genericProc(ClientData clientData, XEvent *eventPtr)
 {
 	Tcl_Interp *interp = clientData;
@@ -28,9 +36,57 @@ static int genericProc(ClientData clientData, XEvent *eventPtr)
 	return 0;
 }
 
+/* set XGrabkeyInfo
+   return TCL_ERROR if fails
+ */
+static int SetXGrabKeyInfo(Tcl_Interp *interp, Tcl_Obj *key, Tcl_Obj *mod,
+                           struct XGrabkeyInfo *info)
+{
+	Tk_Window tkwin = Tk_MainWindow(interp);
+	if (!tkwin) {
+		Tcl_SetObjResult(interp, Tcl_NewStringObj("Tk_MainWindow() return null\n", -1));
+		return TCL_ERROR;
+	}
+	Display *dpy = Tk_Display(tkwin);
+	if (!dpy) {
+		Tcl_SetObjResult(interp, Tcl_NewStringObj("Tk_Display() return null\n", -1));
+		return TCL_ERROR;
+	}
+	Window root = DefaultRootWindow(dpy);
+	Window winid = Tk_WindowId(tkwin);
+	if (!winid) {
+		Tcl_SetObjResult(interp, Tcl_NewStringObj("Tk_WindowId() return null\n", -1));
+		return TCL_ERROR;
+	}
+
+	int keycode;
+	Tcl_GetIntFromObj(interp, key, (int *)&keycode);
+	int min_keycode;
+	int max_keycode;
+	XDisplayKeycodes(dpy, &min_keycode, &max_keycode);
+	if ((keycode < min_keycode) || (keycode > max_keycode)) {
+		Tcl_SetObjResult(interp, Tcl_NewStringObj("keycode out of range", -1));
+		return TCL_ERROR;
+	}
+
+	unsigned int modifiers;
+	Tcl_GetIntFromObj(interp, mod, (int *)&modifiers);
+	if (modifiers > AnyModifier) {
+		Tcl_SetObjResult(interp, Tcl_NewStringObj("modifiers out of range", -1));
+		return TCL_ERROR;
+	}
+
+	info->display = dpy;
+	info->keycode = keycode;
+	info->modifiers = modifiers;
+	info->grab_window = root;
+
+	return TCL_OK;
+}
+
 /* register hotkey */
 static int Hotkey_RegisterCmd(ClientData clientData,
-                      Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
+                              Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
 {
 	/* fprintf(stderr, "hotkey_Cmd: called with %d arguments\n", objc); */
 
@@ -39,39 +95,23 @@ static int Hotkey_RegisterCmd(ClientData clientData,
 		return TCL_ERROR;
 	}
 
-	Tk_Window tkwin = Tk_MainWindow(interp);
-	if (!tkwin) {
-		fprintf(stderr, "Tk_MainWindow() return null\n");
-		return TCL_ERROR;
-	}
-	Display *dpy = Tk_Display(tkwin);
-	if (!dpy) {
-		fprintf(stderr, "Tk_Display() return null\n");
-		return TCL_ERROR;
-	}
-	Window root = DefaultRootWindow(dpy);
-	Window winid = Tk_WindowId(tkwin);
-	if (!winid) {
-		fprintf(stderr, "Tk_WindowId() return null\n");
+	struct XGrabkeyInfo info;
+	if (SetXGrabKeyInfo(interp, objv[1], objv[2], &info) == TCL_ERROR) {
 		return TCL_ERROR;
 	}
 
-	int ret;
-	unsigned int keycode;
-	unsigned int modifiers;
-	Tcl_GetIntFromObj(interp, objv[1], (int *)&keycode);
-	Tcl_GetIntFromObj(interp, objv[2], (int *)&modifiers);
-	XGrabKey(dpy, keycode, modifiers, root, True, GrabModeAsync, GrabModeAsync);
+	XGrabKey(info.display, info.keycode, info.modifiers, info.grab_window,
+	         True, GrabModeAsync, GrabModeAsync);
 
-	Tcl_Obj *key = Tcl_ObjPrintf("%d+%d", keycode, modifiers);
-	ret = Tcl_DictObjPut(interp, hotkeyInfo, key, objv[3]);
+	Tcl_Obj *key = Tcl_ObjPrintf("%d+%d", info.keycode, info.modifiers);
+	int ret = Tcl_DictObjPut(interp, hotkeyInfo, key, objv[3]);
 	if (ret == TCL_ERROR) {
 		return TCL_ERROR;
 	}
-	int size;
-	Tcl_DictObjSize(interp, hotkeyInfo, &size);
 
 	/* create the handler only first time */
+	int size;
+	Tcl_DictObjSize(interp, hotkeyInfo, &size);
 	if (size == 1) {
 		Tk_CreateGenericHandler(genericProc, interp);
 	}
@@ -80,8 +120,8 @@ static int Hotkey_RegisterCmd(ClientData clientData,
 
 /* unregister hotkey */
 static int Hotkey_UnregisterCmd(ClientData clientData,
-                        Tcl_Interp *interp, int objc,
-                        Tcl_Obj *const objv[])
+                                Tcl_Interp *interp, int objc,
+                                Tcl_Obj *const objv[])
 {
 	/* fprintf(stderr, "unhotkey_Cmd: called with %d arguments\n", objc); */
 
@@ -90,33 +130,16 @@ static int Hotkey_UnregisterCmd(ClientData clientData,
 		return TCL_ERROR;
 	}
 
-	Tk_Window tkwin = Tk_MainWindow(interp);
-	if (!tkwin) {
-		fprintf(stderr, "Tk_MainWindow() return null\n");
-		return TCL_ERROR;
-	}
-	Display *dpy = Tk_Display(tkwin);
-	if (!dpy) {
-		fprintf(stderr, "Tk_Display() return null\n");
-		return TCL_ERROR;
-	}
-	Window root = DefaultRootWindow(dpy);
-	Window winid = Tk_WindowId(tkwin);
-	if (!winid) {
-		fprintf(stderr, "Tk_WindowId() return null\n");
+	struct XGrabkeyInfo info;
+	if (SetXGrabKeyInfo(interp, objv[1], objv[2], &info) == TCL_ERROR) {
 		return TCL_ERROR;
 	}
 
-	int ret;
-	unsigned int keycode;
-	unsigned int modifiers;
-	Tcl_GetIntFromObj(interp, objv[1], (int *)&keycode);
-	Tcl_GetIntFromObj(interp, objv[2], (int *)&modifiers);
-	XUngrabKey(dpy, keycode, modifiers, root);
+	XUngrabKey(info.display, info.keycode, info.modifiers, info.grab_window);
 
 	/* no error even if key did not exist */
-	Tcl_Obj *key = Tcl_ObjPrintf("%d+%d", keycode, modifiers);
-	ret = Tcl_DictObjRemove(interp, hotkeyInfo, key);
+	Tcl_Obj *key = Tcl_ObjPrintf("%d+%d", info.keycode, info.modifiers);
+	int ret = Tcl_DictObjRemove(interp, hotkeyInfo, key);
 	if (ret == TCL_ERROR) {
 		return TCL_ERROR;
 	}
